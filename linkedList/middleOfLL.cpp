a905c7c3d424bb18f9dbe255e6d7307c65e55cfd
Ramganga <ram886694@gmail.com>
2025-02-11 22:34:23 +0530

    Middle Of Linked List

---
+++ linkedList/middleOfLL.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+//find the middle of the linked list
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node *middleOfLL(Node *head) // time complexity O(n) and space complexity O(1)
+{ 
+    Node *slow = head;
+    Node *fast = head->next;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+int main()
+{
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    // head->next->next->next->next->next = new Node(6);
+    Node *ans = middleOfLL(head);
+    cout << ans->data;
+    return 0;
+}
