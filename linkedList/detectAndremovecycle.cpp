728dfb4302d8cfc12824fa0087f8dc5b54a7fd8d
Ramganga <ram886694@gmail.com>
2025-02-11 22:47:48 +0530

    Detect Cycle in LL

---
+++ linkedList/detectAndremovecycle.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+// detect cycle in the linked list
+bool detectCycle(Node* head){ //Time complexity O(n) and space complexity O(1)
+    Node* slow = head;
+    Node* fast = head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
+int main(){
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    head->next->next->next->next->next = new Node(6);
+    bool ans = detectCycle(head);
+    if(ans){
+        cout<<"Cycle Detected";
+    }else{
+        cout<<"No Cycle Detected";
+    }
+    return 0;
+}
