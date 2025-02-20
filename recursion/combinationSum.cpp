affc0fbcb16339544437df9ead6547822ec9e016
Ramganga <ram886694@gmail.com>
2025-02-20 23:37:17 +0530

    Combination Sum I

--- recursion/combinationSum.cpp
+++ recursion/combinationSum.cpp
@@ -21,27 +21,7 @@ void solve(vector<int> &nums, int idx, int target, vector<int> &ans, vector<vect
     ans.pop_back();
     solve(nums, idx + 1, target, ans, result);
 }
-{
-    if (idx == nums.size() || target < 0)
-    {
-        return;
-    }
-    if (target == 0)
-    {
-        if (s.find(ans) == s.end())
-        {
-            result.push_back(ans);
-            s.insert(ans);
-        }
-    }
-    ans.push_back(nums[idx]);
-    // single time include
-    solve(nums, idx + 1, target - nums[idx], ans, result);
-    // multiple time include
-    solve(nums, idx, target - nums[idx], ans, result);
-    ans.pop_back();
-    solve(nums, idx + 1, target, ans, result);
-}
+
 int main()
 {
     vector<int> nums = {2, 3, 5};
@@ -49,6 +29,7 @@ int main()
     vector<vector<int>> result;
     int target = 8;
     solve(nums, 0, target, ans, result);
+    cout << "Combinations sum are: " << endl;
     for (auto x : result)
     {
         for (auto y : x)
