67a665102ca9dab5a0c6d8fb67cc490427709890
Ramganga <ram886694@gmail.com>
2025-04-20 02:54:56 +0530

    Tree revision

---
+++ Tree/buildTree.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right  = NULL;
+    }
+};
+static int idx =-1;
+Node* BuildTree(vector<int>preorder){
+    idx++;
+    if(preorder[idx]==-1) return NULL;
+    Node* root = new Node(preorder[idx]);
+    root->left = BuildTree(preorder);
+    root->right = BuildTree(preorder);
+    return root;
+}
+// void preOrder(Node* root){
+//     if(root == NULL){
+//         return;
+//     }
+//     cout << root->data << " ";
+//     preOrder(root->left);
+//     preOrder(root->right);   
+// }
+void levelOrder(Node* root){
+    queue<Node*>q;
+    q.push(root);
+
+    while(!q.empty()){
+        int n = q.size();
+        while(n--){
+            Node *curr = q.front();
+            q.pop();
+            cout << curr->data << " ";
+            if (curr->left)
+            {
+                q.push(curr->left);
+            }
+            if (curr->right)
+            {
+                q.push(curr->right);
+            }
+        }
+        cout << endl;
+    }
+}
+int height(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int leftheight = height(root->left);
+    int rightheight = height(root->right);
+    return max(leftheight,rightheight)+1; 
+}
+int countNode(Node* root){
+    if(root==NULL)return 0;
+    int leftNode = countNode(root->left);
+    int rightNode = countNode(root->right);
+    return leftNode+rightNode+1;
+}
+int sumofNode(Node* root){
+    if(root == NULL) return 0;
+    int leftSum = sumofNode(root->left);
+    int rightSum = sumofNode(root->right);
+    return leftSum+rightSum+root->data;
+}
+int main(){
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root = BuildTree(preorder);  
+    // preOrder(root);
+    levelOrder(root);
+    cout << height(root)<<endl;
+    cout << countNode(root)<<endl;
+    cout << sumofNode(root)<< endl;
+    return 0;
+}
